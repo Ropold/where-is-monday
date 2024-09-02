4132c589ad1df6037f28ef70bc672cf7f221d6ef
Robert Stolz <robert.stolz87@gmail.com>
2024-09-02 17:22:05 +0200

    basic setup

---
+++ db/migrate/20240902150347_create_cats.rb
@@ -0,0 +1,16 @@
+class CreateCats < ActiveRecord::Migration[7.1]
+  def change
+    create_table :cats do |t|
+      t.string :name
+      t.string :description
+      t.string :race
+      t.string :eye_color
+      t.string :color
+      t.string :fur
+      t.boolean :found, default: false
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
