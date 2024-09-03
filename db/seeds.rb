1814caf58b68483a6dad05d6612627cd3cf8e218
Karen Kargosha <Karenkargosha@yahoo.com>
2024-09-03 12:59:13 +0200

    Controller actions done and started view of homepage and show

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,71 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+Cat.destroy_all
+
+Cat.create(
+  name: "Whiskers",
+  description: "A friendly cat",
+  race: "Siamese",
+  eye_color: "Blue",
+  color: "Cream",
+  fur: "Short",
+  found: false,
+  user_id: 1,
+  origin_address: "123 Cat Street",
+  origin_longitude: -122.4194,
+  origin_latitude: 37.7749
+)
+
+Cat.create(
+  name: "Mittens",
+  description: "Loves to play with yarn",
+  race: "Tabby",
+  eye_color: "Green",
+  color: "Gray",
+  fur: "Medium",
+  found: true,
+  user_id: 1,
+  origin_address: "456 Cat Avenue",
+  origin_longitude: -122.4195,
+  origin_latitude: 37.7750
+)
+
+Cat.create(
+  name: "Shadow",
+  description: "Very stealthy",
+  race: "Black Cat",
+  eye_color: "Yellow",
+  color: "Black",
+  fur: "Short",
+  found: false,
+  user_id: 1,
+  origin_address: "789 Cat Boulevard",
+  origin_longitude: -122.4196,
+  origin_latitude: 37.7751
+)
+
+Cat.create(
+  name: "Snowball",
+  description: "Loves the snow",
+  race: "Persian",
+  eye_color: "Blue",
+  color: "White",
+  fur: "Long",
+  found: true,
+  user_id: 1,
+  origin_address: "101 Cat Lane",
+  origin_longitude: -122.4197,
+  origin_latitude: 37.7752
+)
+
+Cat.create(
+  name: "Tiger",
+  description: "Has a fierce look",
+  race: "Bengal",
+  eye_color: "Amber",
+  color: "Orange",
+  fur: "Short",
+  found: false,
+  user_id: 1,
+  origin_address: "202 Cat Road",
+  origin_longitude: -122.4198,
+  origin_latitude: 37.7753
+)
